182dd8d8ebcd261cbc2aa026e1e495b1eef36347
samuelgcabral <samuel.gc1406@gmail.com>
2026-08-11 21:19:45 -0300

    Implementa tela de produto componentizada

---
+++ lib/controllers/product_controller.dart
@@ -0,0 +1,28 @@
+import 'package:avaliacao_componentizacao_stateful_controller/models/product.dart';
+
+class ProductController {
+  ProductController({required this.product});
+  final Product product;
+  String selectedSize = 'M';
+  int quantity = 0;
+  bool isFavorite = true;
+  double get subTotal => product.price * quantity;
+
+  void toggleFavorite() {
+    isFavorite = !isFavorite;
+  }
+
+  void selectSize(String size) {
+    selectedSize = size;
+  }
+
+  void increment() {
+    quantity++;
+  }
+
+  void decrement() {
+    if (quantity > 0) {
+      quantity--;
+    }
+  }
+}
